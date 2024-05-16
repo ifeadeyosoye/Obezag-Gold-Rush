5ff327f0986dde423a45a772a6a67e9f69504bed
ifeadeyosoye <iadeyosoye@keyschool.org>
2024-05-16 15:23:42 -0400

    Creating main file.

--- MainGame.py
+++ MainGame.py
@@ -1,1 +1,26 @@
-im
+# importing modules
+import pygame as py
+from sys import exit
+
+# settings for display window
+py.init()
+screen = py.display.set_mode((800, 400))
+py.display.set_caption('Obezag Gold Rush: Coin Collector')
+pygame_icon = py.image.load('cute-rabbit-characters-png.png')
+py.display.set_icon(pygame_icon)
+
+while True:
+    for event in py.event.get():
+        if event.type == py.QUIT:
+            py.quit()
+            exit()
+    # draw all the elements
+    # update everything
+    py.display.update()
+
+
+
+
+
+
+
